45706dc40037d9960ee6723ad8f5283bf3aa2b95
manuleo03 <manu.leooo03@gmail.com>
2025-03-18 11:32:03 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,239 @@
+const modelli = {
+  "jordan-1": {
+    anno: 1985,
+    descrizione:
+      "Il primo modello in assoluto, denominato 'Notorious', che ribattezza Michael Jordan 'Rookie of the Year'.",
+    nomeModello: "Air Jordan I",
+    storia:
+      "Prezzo di lancio 65$, designer Peter Moore. L’aspetto più interessante del primo modello di Jordan non è tanto la parte tecnica quanto la sua storia. Inizialmente queste sneakers futorono bandite dal campionato NBA, infatti il commissario David Stern, in passato, aveva già bandito una scarpa simile rossa e nera perchè violava le disposizioni riguardanti le divise. Indossarle avrebbe comportato una multa da ben 5000$ a partita! E Nike cosa ha fatto? Ha coperto la multa, ovviamente. Jordan scese in campo con le sue AJI durante tutta la stagione, no matter what.",
+    importanza:
+      "Ha segnato l'inizio della collaborazione tra Michael Jordan e Nike, diventando un'icona nel mondo delle sneakers.",
+    img: "src/jordan-1.jpg",
+  },
+  "jordan-2": {
+    anno: 1986,
+    descrizione:
+      "Un modello interamente prodotto in Italia, noto come 'Italian Stallion', accompagnato dallo slogan 'Rock-a-bye Baby'.",
+    nomeModello: "Air Jordan II",
+    storia:
+      "Prezzo di lancio 100$, designer Peter Moore e Bruce Kilgore. Le Air Jordan II mettono le basi per tutte le altre scarpe della serie: materiali di alta qualità, tecnologia all’avanguardia e design elegante. Questo modello è l’ultimo ideato dai due designers sopra citati, gli stessi che hanno anche creato le Nike Air Force I. Realizzate interamente in Italia, non presentano l’iconico Swoosh. Lo spot commerciale delle AJII raffigura MJ che cammina in aria senza alcuno sforzo.",
+    importanza:
+      "Ha introdotto un design più elegante e sofisticato, enfatizzando la qualità dei materiali.",
+    img: "src/jordan-2.jpg",
+  },
+  "jordan-3": {
+    anno: 1988,
+    descrizione:
+      "Primo modello con il logo Jumpman, noto come 'Gotta Be the Shoes', indossato nel film di Spike Lee 'She's Gotta Have It'.",
+    nomeModello: "Air Jordan III",
+    storia:
+      "Prezzo di lancio 100$, designer Tinker Hatfield. MJ ha dominato la stagione del 1988 e ha vinto il titolo come miglior difensore della lega. Con questa vittoria arriva anche il famosissimo logo Jumpman disegnato da Tinker Hatfield, il designer più prolifico della storia Jordan con ben 21 modelli. Lo spot commerciale era molto divertente e raffigurava MJ sotto Mars Blackmon, personaggio del film di Spike Lee “She’s Gotta Have It”.",
+    importanza:
+      "Ha introdotto il celebre logo Jumpman e un design innovativo che ha consolidato la linea Air Jordan.",
+    img: "src/jordan-3.jpg",
+  },
+  "jordan-4": {
+    anno: 1989,
+    descrizione:
+      "Denominato 'Taking Flight', questo modello ha iniziato a diventare un'icona street, apparendo nel film di Spike Lee 'Do The Right Thing'.",
+    nomeModello: "Air Jordan IV",
+    storia:
+      "Prezzo di lancio 110$, designer Tinker Hatfield. Le esibizioni di MJ che sfidano la gravità sicuramente aiutano a far conoscere la linea di scarpe che da questo momento iniziano anche a diventare icone streetwear fuori dai campi da basket. La particolarità di questo modello sta nel fatto che poteva essere allacciato in ben 18 modi diversi e fu il primo lancio a livello globale del brand. Ancora una volta appare un modello Jordan in un film di Spike Lee. Adesso è il turno di Do The Right Thing",
+    importanza:
+      "Ha rafforzato la presenza delle Air Jordan nella cultura pop e nello streetwear.",
+    img: "src/jordan-4.jpg",
+  },
+  "jordan-5": {
+    anno: 1990,
+    descrizione:
+      "Ispirato agli aerei da combattimento della Seconda Guerra Mondiale, presenta una linguetta riflettente e denti di squalo sulla suola.",
+    nomeModello: "Air Jordan V",
+    storia:
+      "Prezzo di lancio 125$, designer Tinker Hatfield. Per creare questo modello Tinker Hatfield si è ispirato allo stile di gioco di Michael Jordan inserendo forme che ricordano i denti di uno squalo ma anche gli aerei da combattimento della Sconda Guerra Mondiale. Le sneakers presentano anche una suola in gomma trasparente che ricorda le Nike progettate quello stesso anno e indossate dal personaggio Marty McFly in Ritorno al futuro II",
+    importanza:
+      "Ha introdotto elementi di design audaci, diventando un classico tra gli appassionati.",
+    img: "src/jordan-5.jpg",
+  },
+  "jordan-6": {
+    anno: 1991,
+    descrizione:
+      "Caratterizzato da un design pulito e fori per facilitare la calzata, è il modello con cui Jordan ha vinto il suo primo campionato NBA.",
+    nomeModello: "Air Jordan VI",
+    storia:
+      "Prezzo di lancio 125$, designer Tinker Hatfield. Con le sue AJVI ai piedi, Michael Jordan vince il suo primo campionato NBA durante la stagione 1990/91. La silhouette di questo modello si ispira ad una delle macchine sportive del giocatore. Inoltre, queste sneakers sono state le prime a presentare una punta rinforzata e un passante, o spoiler, sul retro per facilitare la calzata.",
+    importanza:
+      "Associato al primo titolo NBA di Jordan, è un modello molto significativo nella sua carriera.",
+    img: "src/jordan-6.jpg",
+  },
+  "jordan-7": {
+    anno: 1992,
+    descrizione:
+      "Presenta un design ispirato all'arte africana e una costruzione senza elementi visibili di Air, indossato da Jordan durante le Olimpiadi di Barcellona.",
+    nomeModello: "Air Jordan VII",
+    storia:
+      "Prezzo di lancio 125$, designer Tinker Hatfield. Dopo la stagione che ha visto i Bulls vincitori al campionato NBA, arriva l’oro alle Olimpiadi di Barcellona del 1992 e le Air Jordan VII si fanno conoscere come parte integrante del “Dream Team”. Il modello originale presenta il numero 23 ma c’è anche una colorazione speciale con il numero 9, ovvero il numero di MJ vestendo la divisa USA ai Giochi Olimpici. Tutte le colourways si ispirano a stampe tribali africane. Nello spot commerciale appare Bugs Bunny per la celebre collaborazione nel film Space Jam.",
+    importanza:
+      "Ha segnato un cambiamento nel design e ha accompagnato Jordan durante la vittoria olimpica con il Dream Team.",
+    img: "src/jordan-7.jpg",
+  },
+  "jordan-8": {
+    anno: 1993,
+    descrizione:
+      "Conosciuto per le sue cinghie incrociate sul collo del piede e un'imbottitura extra, è l'ultimo modello indossato da Jordan prima del suo primo ritiro.",
+    nomeModello: "Air Jordan VIII",
+    storia:
+      "Prezzo di lancio 125$, designer Tinker Hatfield. Le AJVIII hanno debuttato lo stesso anno in cui i Chicago Bulls sono diventati leggenda vincendo per il terzo anno consecutivo il campionato NBA, mentre MJ guidava la classifica per il settimo anno consecutivo. Questo modello è l’unico della linea Air Jordan che presenta cinturini incrociati sopra i lacci e la colorazione Concorde / Aqua – Tone – Black lo ha reso un modello iconico degli anni ’90.",
+    importanza:
+      "Rappresenta la fine di un'era prima del primo ritiro di Jordan, con un design distintivo.",
+    img: "src/jordan-8.jpg",
+  },
+  "jordan-9": {
+    anno: 1994,
+    descrizione:
+      "Primo modello rilasciato durante il ritiro di Jordan, presenta una suola con iscrizioni in diverse lingue, simboleggiando la globalizzazione del brand.",
+    nomeModello: "Air Jordan IX",
+    storia:
+      "Prezzo di lancio 125, designer Tinker Hatfield. Questo è l’anno in cui Jordan annuncia il suo ritiro, in parte dovuto all’omicidio del padre dello stesso anno. Nonostante la pausa di Jordan, il successo delle omonime sneakers non poteva essere fermato e così il brand lancia questa nuova silhouette come omaggio a diverse culture perchè tutti, in ogni parte del mondo, volevano essere come MJ e così questo modello segna la globalizzazione della marca.",
+    importanza:
+      "Simbolizza l'espansione globale del marchio Air Jordan durante l'assenza di MJ dai campi NBA.",
+    img: "src/jordan-9.jpg",
+  },
+  "jordan-10": {
+    anno: 1995,
+    descrizione:
+      "Disegnato durante il ritiro di Jordan, presenta una lista dei suoi successi sulla suola e un design semplice e pulito.",
+    nomeModello: "Air Jordan X",
+    storia:
+      "Prezzo di lancio 125$, designer Tinker Hatfield. Durante la pausa dal basket, il giocatore professionista gioca brevemente a baseball. Nel mentre Nike pensava che la sua carriera sul parquet fosse ufficialmente finita e così questo modello vuole omaggiare il suo percorso. Ma già nel ’95 Michael Jordan manda un fax dicendo “I’m back” (Sono tornato) e mette ai piedi le Air Jordan X.",
+    importanza:
+      "Celebra i traguardi di Jordan, ed è il modello che ha segnato il suo ritorno in NBA.",
+    img: "src/jordan-10.jpg",
+  },
+  "jordan-11": {
+    anno: 1996,
+    descrizione:
+      "Famoso per l'uso del materiale in vernice sulla tomaia, è stato indossato da Jordan nel film 'Space Jam' e durante la stagione del record 72-10 dei Bulls.",
+    nomeModello: "Air Jordan XI",
+    storia:
+      "Prezzo di lancio 125$, designer Tinker Hatfield. THE GOAT è ufficialmente tornato per giocare una delle stagioni più incredibili di sempre e Tinker Hatfield voleva una silhouette che incarnasse proprio prestazioni e aerodinamica, simboli dello stile di gioco di Jordan. Così venne introdotto l’inserto in pelle lucida, un materiale mai usato prima nel basket.",
+    importanza:
+      "Considerato uno dei modelli più iconici, ha combinato eleganza e performance in modo innovativo.",
+    img: "src/jordan-11.jpg",
+  },
+  "jordan-12": {
+    anno: 1997,
+    descrizione:
+      "Ispirato al design di un ventaglio giapponese, presenta linee pulite e materiali di alta qualità, indossato da Jordan durante la 'Flu Game'.",
+    nomeModello: "Air Jordan XII",
+    storia:
+      "Prezzo di lancio 135$, designer Tinker Hatfield. È il modello indossato nel leggendario “Flu Game” in cui Jordan ha registrato 38 punti nonostante la febbre alta ed è anche considerato il modello più resistente della linea. L’ispirazione per la silhouette arriva dalle scarpe Nisshoki e dalla bandiera giapponese, infatti, le cuciture laterali ricordano i raggi del sole.",
+    importanza:
+      "Associato a una delle performance più memorabili di Jordan, rappresenta resilienza e determinazione.",
+    img: "src/jordan-12.jpg",
+  },
+  "jordan-13": {
+    anno: 1998,
+    descrizione:
+      "Ispirato alla pantera, con una suola che richiama la zampa dell'animale e un ologramma 'occhio di gatto' sul tallone.",
+    nomeModello: "Air Jordan XIII",
+    storia:
+      "Prezzo di lancio 150$, designer Tinker Hatfield. Michael Jordan aveva molti soprannomi tra cui “Air Jordan”, “His Airness”, “THE GOAT”, “Jumpman” ma anche “Black Cat”. Questo modello vuole proprio rendere omaggio alla sua prodezza furtive, come quella di una pantera nera. Tinker Hatfield ha progettato la suola in modo che sembrasse la zampa di una pantera e ha posizionato un occhio olografico verde con il numero 23.",
+    importanza:
+      "Ha accompagnato Jordan durante la sua ultima stagione completa con i Bulls, culminando nel sesto titolo NBA.",
+    img: "src/jordan-13.jpg",
+  },
+  "jordan-14": {
+    anno: 1999,
+    descrizione:
+      "Ispirato alla Ferrari 550 Maranello, presenta linee aerodinamiche e dettagli che richiamano l'auto sportiva.",
+    nomeModello: "Air Jordan XIV",
+    storia:
+      "Prezzo di lancio 150$, designer Tinker Hatfield. Quando il designer consegnò questo prototipo a Jordan gli chiede di non indossarlo e di non rivelarlo, ma il giocatore non poté resistere e indossò queste sneakers alle finali contro gli Utah Jazz mettendo a punto il suo “Last Shot” e portandosi a casa l’ultimo titolo NBA come Chicago Bull. Questo modello si ispira alla Ferrari 550 M e su ogni scarpa sono presenti 7 loghi Jumpman, 14 quindi se consideriamo ogni paio.",
+    importanza:
+      "Indossato da Jordan durante il suo 'Last Shot' con i Bulls, è un modello leggendario nella storia del basket.",
+    img: "src/jordan-14.jpg",
+  },
+  "jordan-15": {
+    anno: 2000,
+    descrizione:
+      "Design futuristico ispirato agli aerei da combattimento X-15, con una linguetta sporgente sul tallone e una costruzione aggressiva.",
+    nomeModello: "Air Jordan XV",
+    storia:
+      "Prezzo del lancio 150$, designer Tinker Hatfield. Jordan si ritira per la seconda volta alla fine della stagione 1997/98. Questo è il primo modello che non verrà indossato dal giocatore infatti, nello spot pubblicitario, MJ abbraccia il suo nuovo ruolo al di fuori del campo per diventare un uomo d’affari. L’annuncio presenta giocatori emergenti come Derek Jeter e Ray Allen. Questa è anche la 13esima sneaker disegnata da Tinker Hatfield e sarà l’ultima fino alle AJXX. Le AJXV presentano una tomaia in fibra intrecciata Kevlar e una linguetta sporgente.",
+    importanza:
+      "Primo modello rilasciato dopo il secondo ritiro di Jordan, rappresenta un nuovo capitolo per il brand.",
+    img: "src/jordan-15.jpg",
+  },
+  "jordan-16": {
+    anno: 2001,
+    descrizione:
+      "Primo modello dell'era post-Jordan, caratterizzato da una copertura rimovibile sulla parte superiore e dettagli ispirati alle scarpe classiche.",
+    nomeModello: "Air Jordan XVI",
+    storia:
+      "Prezzo di lancio 160$, designer Wilson Smith III. Per la prima volta dopo 10 anni, Jordan ha un nuovo designer. Questo modello simbolizza il cambiamento di MJ da giocatore a presidente degli Washington Wizard con un inserto removibile che appunto trasforma la sneaker da una scarpa da basket a una scarpa streetwear. Nella stessa stagione Michael Jordan annuncia il suo rientro in campo come giocatore degli Washington Wizards. Lo spot pubblicitario, con le musiche di Mos Def, vuole sottolineare che il brand Jordan è molto più che una semplice marca.",
+    importanza:
+      "Segna una nuova era per il brand, con un design che unisce stile casual e sportivo.",
+    img: "src/jordan-16.jpg",
+  },
+  "jordan-17": {
+    anno: 2002,
+    descrizione:
+      "Ispirato alla musica jazz, presenta una custodia metallica e un sistema di ammortizzazione migliorato.",
+    nomeModello: "Air Jordan XVII",
+    storia:
+      "Prezzo di lancio 200$, designer Wilson Smith III. Ancora una volta l’ispirazione per la silhouette di un modello del brand viene da una delle macchine di Michael Jordan, in questo caso dalla sua Aston Martin mentre le note musicali rappresentano la passione del giocatore per il jazz, che fa da sottofondo anche allo spot commerciale. Avete notato che hanno un prezzo di ben 200$? Giustificato dal packaging in metallo e da un CD-ROM incluso.",
+    importanza: "Associato al ritorno di Jordan con i Washington Wizards.",
+    img: "src/jordan-17.jpg",
+  },
+  "jordan-18": {
+    anno: 2003,
+    descrizione:
+      "Ispirato alle auto di lusso italiane, con pelle scamosciata e dettagli sofisticati.",
+    nomeModello: "Air Jordan XVIII",
+    storia:
+      "Prezzo di lancio 175$, designer Tate Kuerbis. Ecco un altro modello che prende ispirazione dalle macchine sportive di THE GOAT: il tallone in gomma è ispirato alle scarpe da racing mentre le cuciture si ispirano all’eleganza delle scarpe italiane. Come tocco finale di stile, il packaging della colorazione OG Black / Sport Royal viene con un asciugamano, con una spazzola e con un manuale di guida. Seguendo l’ultimo e definitivo ritiro di Jordan, la pubblicità delle AJXVIII ripercorre le tappe della carriera del giocatore e termina con “What is love? Love is playing every game as if it’s your last.” ( Cos’è l’amore? L’amore è giocare ogni partita come se fosse l’ultima)",
+    importanza: "Ultimo modello indossato da Jordan in NBA.",
+    img: "src/jordan-18.jpg",
+  },
+};
+
+// Genera il catalogo con Bootstrap Cards
+function generaCatalogo() {
+  const catalogoElement = document.getElementById("catalogo");
+
+  for (let modello in modelli) {
+    const modelloData = modelli[modello];
+
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("col-md-4", "mb-4");
+
+    cardDiv.innerHTML = `
+      <div class="card h-100 ingrandita" data-modello="${modello}" style="cursor: pointer;">
+        <img src="${modelloData.img}" class="card-img-top" alt="${modelloData.nomeModello}">
+        <div class="card-body">
+          <h5 class="card-title piccola">${modelloData.nomeModello}</h5>
+          <p class="card-text piccola">${modelloData.descrizione}</p>
+        </div>
+      </div>
+    `;
+
+    const cardElement = cardDiv.querySelector(".card");
+
+    cardElement.addEventListener("click", function () {
+      if (modello === "jordan-1") {
+        // Solo la Jordan 1 porta al dettaglio
+        window.location.href = `model-detail.html?modello=${modello}`;
+      } else {
+      }
+    });
+
+    catalogoElement.appendChild(cardDiv);
+  }
+}
+
+// Esegue al caricamento della pagina
+window.onload = function () {
+  if (document.getElementById("catalogo")) {
+    generaCatalogo();
+  }
+};
